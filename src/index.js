7c81a48b0632ef010d581ee77ca7c8d7daea4350
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2023-07-10 14:54:48 -0300

    feat: Subindo criação de conta

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const express = require('express');
+const { v4: uuidv4 }  = require('uuid');
 
 const app = express();
+
+app.use(express.json())
+
+const clientes = [];
+
+/**
+ * DAdos da conta
+ * CPF - string
+ * nome - string
+ * id - uuid
+ * extrato - array
+ */
+
+/**Método para criar conta */
+app.post("/conta", (request,response) => {
+    const { cpf, nome } = request.body;
+    const id = uuidv4();
+    clientes.push({cpf, nome, id, extrato:[]});
+    const messagem = 'Conta criada com sucesso!!'
+    return response.status(201).send(messagem)
+}) 
 
 app.listen(3333);
